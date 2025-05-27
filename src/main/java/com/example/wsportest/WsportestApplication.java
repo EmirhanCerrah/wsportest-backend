788e7e0ddfead585b895c037f17182bb32f0a544
Emirhan Cerah <71515401+EmirhanCerrah@users.noreply.github.com>
2025-05-27 16:16:42 +0300

    Add files via upload

---
+++ src/main/java/com/example/wsportest/WsportestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wsportest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsportestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsportestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wsportest/WsportestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsportest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsportestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
