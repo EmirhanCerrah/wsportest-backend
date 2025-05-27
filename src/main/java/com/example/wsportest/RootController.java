850b861311b8ccc6d7e7e05260fa45a034698ef6
Emirhan Cerah <71515401+EmirhanCerrah@users.noreply.github.com>
2025-05-27 16:55:10 +0300

    Create RootController.java

---
+++ src/main/java/com/example/wsportest/RootController.java
@@ -0,0 +1,12 @@
+package com.example.wsportest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public String home() {
+        return "Backend çalışıyor!";
+    }
+}
